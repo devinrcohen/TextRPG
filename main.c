6b57b17079fd0fe62c7c8fc67c166f1e0954c44a
devinrcohen <devin.r.cohen@protonmail.com>
2026-02-13 00:05:41 -0800

    Game loop added, test prints removed

--- main.c
+++ main.c
@@ -9,10 +9,11 @@
 
 int main(void) {
     Game game;
+    game.settings.mainWindow = stdscr;
     game_reset_to_new_run(&game);
 
     GameState st = ST_TITLE;
-    ui_init();
+    ui_init(&game.settings);
 
     /* game loop */
     while (st != ST_QUIT) {
@@ -24,46 +25,6 @@ int main(void) {
             default:            st = ST_QUIT;                break;
         }
     }
-
-    /*old stuff, for reference
-    int yMax, xMax;
-    getmaxyx(stdscr, yMax, xMax);
-
-    attron(A_UNDERLINE | COLOR_PAIR(3) | A_BOLD);
-    wPrintToCenter_Offset(stdscr, -1, "Welcome to [GAME TITLE HERE]");
-    attroff(A_UNDERLINE);
-    refresh(); // must flush before delay
-    delay_ms(1500);
-
-    wPrintToCenter_Offset(stdscr, 1, "Press [Enter] to continue");
-    attroff(A_UNDERLINE | A_BOLD);
-    refresh();
-
-    attron(COLOR_PAIR(1));
-    mvaddstr(yMax-2, 0, "Enter your name: ");
-
-    move(yMax-1, 0);
-    char name[20];
-
-    CURSOR_STRONG
-    getstr(name);
-    CURSOR_OFF
-
-    attron(COLOR_PAIR(4));
-    wPrintToCenterY(stdscr, 1, name);
-    attroff(COLOR_PAIR(4));
-
-    attron(COLOR_PAIR(1)); // Blue-on-black
-    wPrintToCenterX(stdscr, 1, name);
-    attroff(COLOR_PAIR(1));
-
-    attron(COLOR_PAIR(2)); // Red-on-black
-    wPrintLeft(stdscr, yMax*3/4, name);
-    attroff(COLOR_PAIR(2));
-    attron(COLOR_PAIR(3));
-    wPrintRight(stdscr, yMax*1/4, name);
-    attroff(COLOR_PAIR(3));
-    refresh();*/
 
     ui_shutdown();
 
