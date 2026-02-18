1a423d3ee085edc45fc4e64d5fa092d702e973d5
devinrcohen <devin.r.cohen@protonmail.com>
2026-02-17 22:35:55 -0800

    generalized window scaling

--- main.c
+++ main.c
@@ -22,8 +22,8 @@ int main(void) {
  *|  |____________|
  *|__|___dialog___|
  */
-    int rowsMain = game.ui.settings.yMax;
-    int colsMain = game.ui.settings.xMax;
+    int rowsMax = game.ui.settings.yMax;
+    int colsMax = game.ui.settings.xMax;
 
     float intro_height_ratio = 1.0f;
     float intro_width_ratio  = 1.0f;
@@ -31,16 +31,31 @@ int main(void) {
     float main_height_ratio = 0.75f;
     float main_width_ratio  = 0.75f;
 
+    int rowsMain = main_height_ratio * rowsMax;
+    int colsMain = main_width_ratio * colsMax;
+    int y0main = 0;
+    int x0main = colsMax - colsMain;
+
     float status_height_ratio = 1.0f;
-    float status_width_ratio  = 0.75f;
+    float status_width_ratio  = 0.25f;
+
+    int rowsStatus = status_height_ratio * rowsMax;
+    int colsStatus = status_width_ratio * colsMax;
+    int y0status = 0;
+    int x0status = 0;
 
     float dialog_height_ratio = 0.25f;
     float dialog_width_ratio  = 0.75f;
 
-    WINDOW* win_intro  = newwin(rowsMain, colsMain, 0, 0);
-    WINDOW* win_main   = newwin((3*rowsMain/4), (3*colsMain/4), 0, colsMain/4);
-    WINDOW* win_status = newwin(rowsMain, colsMain/4, 0, 0);
-    WINDOW* win_dialog = newwin(rowsMain/4+1, (3*colsMain)/4, (3*rowsMain)/4, colsMain/4);
+    int rowsDialog = dialog_height_ratio * rowsMax;
+    int colsDialog = dialog_width_ratio * colsMax;
+    int y0dialog = rowsMax - rowsDialog;
+    int x0dialog = colsMax - colsDialog;
+
+    WINDOW* win_intro  = newwin(rowsMax, colsMax, 0, 0);
+    WINDOW* win_main   = newwin(rowsMain, colsMain, y0main, x0main);
+    WINDOW* win_status = newwin(rowsStatus, colsStatus, y0status, x0status);
+    WINDOW* win_dialog = newwin(rowsDialog+1, colsDialog, y0dialog-1, x0dialog);
 
 
     PANEL* pan_intro  = ui_add_panel(&game.ui, win_intro,  INTRO_INDEX);
@@ -48,17 +63,19 @@ int main(void) {
     PANEL* pan_status = ui_add_panel(&game.ui, win_status, STATUS_INDEX);
     PANEL* pan_dialog = ui_add_panel(&game.ui, win_dialog, DIALOG_INDEX);
 
+    point loc_box = {2, 1};
+    SelectBox select_box = new_selectbox(win_status, &game.ui, loc_box, "Yes", "No");
 
-    box(win_main, '*', '/');
-    box(win_status, '|', '-');
-    box(win_dialog, '\\', '~');
+    box(win_main, 0, 0);
+    box(win_status, 0, 0);
+    box(win_dialog, 0, 0);
 
     top_panel(pan_intro);
     update_panels();
     doupdate();
 
     GameState st = ST_TITLE;
-
+    game_reset_to_new_run(&game);
     /* game loop */
     while (st != ST_QUIT) {
         switch (st) {
